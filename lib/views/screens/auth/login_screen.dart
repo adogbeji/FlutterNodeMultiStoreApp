50508a2aacdc25924d4ea292d1379862e26f9874
adogbeji <baka_1993@yahoo.co.uk>
2024-10-23 20:25:10 +0100

    Start using google fonts in login screeen

--- lib/views/screens/auth/login_screen.dart
+++ lib/views/screens/auth/login_screen.dart
@@ -1,16 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Log In', style: TextStyle(color: Colors.black),),
+            Text(
+              'Log In',
+              style: GoogleFonts.getFont(
+                'lato',
+                fontSize: 23,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 0.2,
+              ),
+            ),
           ],
         ),
       ),
